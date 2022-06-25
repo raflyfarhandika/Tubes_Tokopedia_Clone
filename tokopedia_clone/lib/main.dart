ca9099ccb2d3c56b027bf1f04bd7dc9c579e124f
raflyfarhandika <raflyfarhandika@gmail.com>
2022-06-25 22:33:25 +0700

    init Tubes

---
+++ tokopedia_clone/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:tokopedia_clone/screen/cart.dart';
+import 'package:tokopedia_clone/screen/loginPage.dart';
+
+Future<void> main() async{
+  WidgetsFlutterBinding();
+  await Firebase.initializeApp();
+  SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
+      runApp(MyApp());
+    });
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tokopedia Demo',
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      debugShowCheckedModeBanner: true,
+      home: LoginPage(),
+    );
+  }
+}
